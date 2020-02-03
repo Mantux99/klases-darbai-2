a0d39dbcd079fc3be048ab5920d8f9b1134c6881
Mantux99 <cerkesas.mantas@gmail.com>
2020-02-03 10:17:25 +0100

    antra uzduotis

--- index.php
+++ index.php
@@ -7,3 +7,14 @@ $date = date('Y/m/d');
 $result = $string . $date;
 
 print $result;
+
+echo "<br>";
+ 
+//2. sukurti sakini is 5< zodziu. po kiekvieno zodzio yra padetas taskas. 
+//Jums reikia naudojant bazines php funkcijas tuos taskus pakeisti i tarpus.
+
+$tekstas = 'sudareme sakini is penkiu zodziu';
+$str = str_replace(' ', '.', $tekstas);
+print $str;
+
+//
